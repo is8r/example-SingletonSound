8270d9482c9046b209743aa251c9c02dc78e9557
Yu Ishihara <ishihara.yu@gmail.com>
2020-11-09 00:39:33 +0900

    Update

---
+++ Assets/SoundManager/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿/*
+
+hotwto:
+SoundManager.Instance.Play(0);
+
+*/
+
+using UnityEngine;
+
+namespace SoundManagement
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundManager : SoundManagement.SingletonMonoBehaviour<SoundManager>
+    {
+        //音源を登録する配列
+        public AudioClip[] audios;
+
+        //外部から呼び出す
+        public void Play(int n = 0)
+        {
+            //オーディオソースを呼び出す
+            AudioSource audiosource = GetComponent<AudioSource>();
+
+            //再生中のスクリプトを停止する
+            audiosource.Stop();
+
+            //オーディオソースを再生する
+            audiosource.PlayOneShot(audios[n]);
+        }
+    }
+}
